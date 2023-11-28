bab397361c4f3ed1f0654ee1c7c269f7c91d7cbe
JM <jmdsegismundo@gmail.com>
2023-11-28 20:39:01 +0800

    feat: css modules

--- hw-react-pokedex/src/components/FetchData.jsx
+++ hw-react-pokedex/src/components/FetchData.jsx
@@ -1,9 +1,10 @@
 import { useState, useEffect } from 'react';
+import styles from './FetchData.module.css';
 
 const typeStyles = {
   fire: { backgroundColor: 'rgb(240, 128, 48)' },
   water: { backgroundColor: 'rgb(24,71,215)' },
-  grass: { backgroundColor: 'rgb(54,145,67)' },
+  grass: { backgroundColor: 'rgb(80, 199, 99)' },
   poison: { backgroundColor: 'rgb(160,64,160)' },
   bug: { backgroundColor: 'rgb(155,204,80)' },
   flying: { backgroundColor: 'rgb(48,167,215)' },
@@ -23,6 +24,8 @@ const typeStyles = {
 
 function FetchData() {
   const [pokemon, setPokemon] = useState([]);
+  const [offset, setOffset] = useState(0);
+  const [limit, setLimit] = useState(20);
 
   async function fetchPokeImage(id) {
     try {
@@ -60,7 +63,7 @@ function FetchData() {
   async function fetchPokeData() {
     try {
       const response = await fetch(
-        'https://pokeapi.co/api/v2/pokemon?limit=151&offset=0'
+        `https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`
       );
       const result = await response.json();
 
@@ -74,34 +77,58 @@ function FetchData() {
         })
       );
 
-      setPokemon(pokemonList);
+      setPokemon([...pokemon, ...pokemonList]);
     } catch (error) {
       console.log(error.message);
     }
   }
 
+  const loadMore = () => {
+    setOffset(offset + limit);
+  };
+
   useEffect(() => {
     fetchPokeData();
-  }, []);
+  }, [offset, limit]);
 
   return (
     <>
-      <div>NAV</div>
-      <div className="pokemon-card">
+      <div className={styles.navbar}>NAV</div>
+      <div className={styles.pokemonCard}>
         {pokemon.map((item, index) => (
-          <div className="pokemon" key={index}>
-            <div className="pokemon-number">#{item.number}</div>
-            <img src={item.sprite} alt={item.name} />
-            <p className="pokemon-name">{item.name}</p>
-            <div>
-              {item.type.map((type, typeIndex) => (
-                <div key={typeIndex} style={typeStyles[type]}>
-                  {type}
-                </div>
-              ))}
+          <div className={styles.pokemonBorder} key={index}>
+            <div className={styles.pokemonNumber}>#{item.number}</div>
+            <div className={styles.pokemon} key={index}>
+              <img
+                className={styles.pokemonImage}
+                src={item.sprite}
+                alt={item.name}
+              />
+              <div className={styles.pokemonName}>{item.name}</div>
+              <div>
+                {item.type.map((type, typeIndex) => (
+                  <span
+                    className={styles.pokemonType}
+                    key={typeIndex}
+                    style={typeStyles[type]}
+                  >
+                    {type}
+                  </span>
+                ))}
+              </div>
             </div>
           </div>
         ))}
+      </div>
+
+      <button className={styles.loadMoreButton} onClick={loadMore}>
+        Load More
+      </button>
+
+      <div className={styles.footer}>
+        <span>© Jhune Michael Segismundo</span>
+        <br />
+        <span>Made using React</span>
       </div>
     </>
   );
